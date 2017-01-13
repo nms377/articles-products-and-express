ddb74efbbd09ac2b593186622f73e85a06cf24e4
nms377 <nms377@nyu.edu>
2017-01-12 18:15:16 -1000

    Created a routes directory and added an article.js and products.js files. This is where I will do my POST, GET, PUT, DELETE methods.

---
+++ routes/articles.js
@@ -0,0 +1,9 @@
+//put router modules for db/articles.js here
+
+
+//Export this to db/articles.js
+// module.exports = something
+
+const server = require('../server');
+const supertest = require('supertest');
+const chai = require('chai');
---
+++ routes/products.js
@@ -0,0 +1,8 @@
+//put router modules for db/products.js here
+
+//Export this to db/products.js
+// module.exports = something
+
+const server = require('../server');
+const supertest = require('supertest');
+const chai = require('chai');
