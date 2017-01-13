48e06349107024766e38a3b2f60a3e10092907c1
nms377 <nms377@nyu.edu>
2017-01-12 19:04:43 -1000

    Initially added my express.Router stuff in this file, but realized I needed to do the opposite.

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
-const handlebars = require('express-handlebars');
+
+// const handlebars = require('express-handlebars');
 const products = require('./routes/products');
-const articles = require('./routes/articles');
+// const articles = require('./routes/articles');
+
+app.use('/products', products);
 
 module.exports = app;
