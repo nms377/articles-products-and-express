330603953dc1b58516bc4b5b382db1439e9bcf9b
nms377 <nms377@nyu.edu>
2017-01-12 19:45:43 -1000

    Imported body-parser.

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+const bs = require('body-parser');
 
 // const handlebars = require('express-handlebars');
 const products = require('./routes/products');
 // const articles = require('./routes/articles');
+
+//parse application/x-www-form-urlencoded
+app.use(bs.urlencoded({ extended: false }));
 
 app.use('/products', products);
 
