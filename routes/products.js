d4af8f0ba05919c211ccd67959c4e6d199357fe7
nms377 <nms377@nyu.edu>
2017-01-14 09:46:39 -1000

    Added multiple products then used my PUT method to make sure it worked, and found that I was changing the name of all the products in the array. Declared the specific ID I was looking for as variable productID and put it in my for loop that if the product's ID striclty matches the ID of productID then change that product's name.

--- routes/products.js
+++ routes/products.js
@@ -24,14 +24,18 @@ router.route('/')
 
 router.route('/:id')
 	.put((req, res) => {
+	let productID = req.body.id;
+	//loop through the array of objects
 	for(let i=0; i < productsArr.length; i++){
-		if(productsArr.indexOf(i)){
-			productsArr[i].name = req.body.name;
+		//loop through array and find the id that matches the id if it matches
+		if(productsArr[i].id === productID){
+				//then change the product name in that object
+				productsArr[i].name = req.body.name;
 		}
-
+		console.log(productsArr[i].name);
 	}
-	console.log(productsArr);
-	res.send('Test');
+	// console.log(productsArr);
+	res.send('test');
 });
 
 module.exports = router;
