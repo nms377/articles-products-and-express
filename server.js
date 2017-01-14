3a880ea639022cac65f3649775a172f979fd524f
nms377 <nms377@nyu.edu>
2017-01-14 11:48:08 -1000

    Configured handlebars.

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const app = express();
+const handlebars = require('express-handlebars');
 const bs = require('body-parser');
 
-// const handlebars = require('express-handlebars');
+const hbs = handlebars.create({
+	extname: '.hbs',
+	defaultLayout: 'app'
+});
 const products = require('./routes/products');
 // const articles = require('./routes/articles');
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
 
 //parse application/x-www-form-urlencoded
 app.use(bs.urlencoded({ extended: false }));
 
 app.use('/products', products);
+
+app.get('/', (req, res) => {
+	res.render('index');
+});
 
 module.exports = app;
