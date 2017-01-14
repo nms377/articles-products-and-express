2bb726e476b256bb1fcb7b2704509945a8df9fd9
nms377 <nms377@nyu.edu>
2017-01-13 23:02:57 -1000

    Using POST method, created an id on server side and added it to the product. Initially tried to use push method, then apply, which did not work. The id will increase by one every time a product is added.

--- routes/products.js
+++ routes/products.js
@@ -5,6 +5,7 @@ const app = express();
 const router = express.Router();
 
 var productsArr = [];
+var id = 0;
 
 //define the products route
 router.route('/')
@@ -13,8 +14,11 @@ router.route('/')
 	res.json({"products": productsArr});
 })
 	.post((req, res) => {
-		productsArr.push(req.body);
-		console.log('Products Added');
+		let product =req.body;
+		product.id = `${id}`;
+		productsArr.push(product);
+		console.log(productsArr);
+		id++;
 		res.send('Let\'s create a new product!');
 })
 	.put((req, res) => {
