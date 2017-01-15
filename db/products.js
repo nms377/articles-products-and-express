c79f6e009d67a931cce7ec3a42e97d77fb01cc3a
nms377 <nms377@nyu.edu>
2017-01-14 16:31:03 -1000

    used ParseInt to get my id to match my id in my array.

--- db/products.js
+++ db/products.js
@@ -1,24 +1,40 @@
 module.exports = (function(){
 
 	let product = {};
-	var _productsArr = [];
+	var productsArr = [];
 	var id = 0;
 
 	function _all(){
-		return _productsArr;
+		return productsArr;
 	}
 
 	function _add(data){
 		data['id'] = id;
-		_productsArr.push(data);
+		productsArr.push(data);
 		id++;
-		console.log(_productsArr);
+		console.log(productsArr);
+	}
+
+	function _editProductById(data, id){
+
+		let productName = data.name;
+
+		for(let i=0; i < productsArr.length; i++){
+			console.log(productsArr[i].id);
+			if(productsArr[i].id === parseInt(id)){
+				productsArr[i].name = productName;
+				console.log(productsArr[i]);
+			}else{
+				console.log('error');
+			}
+		}
+
 	}
 
 	return {
 		all: _all,
 		add: _add,
-		// editProductById: _editProductById,
+		editProductById: _editProductById,
 		// deleteProductById: _deleteProductById
 	};
 
