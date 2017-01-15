41a62ed3928578a74eb20e55378731f8da8fa7f3
nms377 <nms377@nyu.edu>
2017-01-14 16:41:10 -1000

    Passed in argument req.params.id in the products.deleteProductById function inside the delete method.

--- routes/products.js
+++ routes/products.js
@@ -17,25 +17,13 @@ router.route('/')
 		res.redirect(201, '/');
 });
 
-//edits product
-// function editProduct(productsArr, productID, product, newProductName){
-// 	for(let i=0; i < productsArr.length; i++){
-// 		if(productsArr[i].id === productID){
-// 			productsArr[i].name = newProductName;
-// 		}
-// 	}
-// }
-
 router.route('/:id')
 	.put((req, res) => {
 		products.editProductById(req.body, req.params.id);
 		res.redirect(201, '/:id');
 })
 	.delete((req,res) => {
-		let newProductName = req.body.name;
-		let productID = req.body.id;
-		let newProduct = deleteProduct(productsArr, productID, newProductName);
-		console.log(productsArr);
+		products.deleteProductById(req.params.id);
 		res.redirect(201, '/');
 });
 
