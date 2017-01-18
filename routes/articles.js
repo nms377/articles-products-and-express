d8d89e0e725a409228bb56171412afa591decab5
nms377 <nms377@nyu.edu>
2017-01-18 07:54:10 -1000

    Created route for /articles.

--- routes/articles.js
+++ routes/articles.js
@@ -1,9 +1,26 @@
-//put router modules for db/articles.js here
-
-
-//Export this to db/articles.js
-// module.exports = something
-
+//imports
 const server = require('../server');
-const supertest = require('supertest');
-const chai = require('chai');
+const express = require('express');
+const articles = require('../db/articles');
+
+
+//invocations
+const app = express();
+const router = express.Router();
+
+router.route('/')
+	//View Product List
+	.get((req,res) => {
+	console.log('wat');
+	res.render('articles/index');
+})
+	//When you add a new product, this redirects you the product page to view your product list
+	.post((req, res) => {
+			let addArticle = articles.add(req.body);
+			let store = {
+				addArticle: addArticle
+			};
+			res.redirect('/articles');
+});
+
+module.exports = router;
