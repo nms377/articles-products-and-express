a60af9cfc147f896a7e524d89c7cd01ed14a2b32
nms377 <nms377@nyu.edu>
2017-01-18 07:54:42 -1000

    Couldn't get the server to connect to my articles route file so I copy and pasted this code and updated it to reflect articles.

--- routes/products.js
+++ routes/products.js
@@ -34,7 +34,7 @@ router.route('/:id')
 	//View Product Based on Id
 	.get((req,res) => {
 		let productId = products.editProductById(req.body, req.params.id);
-		res.redirect(303, '/products/${req.params.id}/edit');
+		res.redirect(303, `/products/${req.params.id}/edit`);
 })
 	//Edits Product and redirects client to Product page to view product based on Id
 		.put((req, res) => {
@@ -49,11 +49,11 @@ router.route('/:id/edit')
 	//View Product Page to Edit Product Based on Id
 	.get((req, res) => {
 		let productId = products.getProductById(req.body, req.params.id);
-		let store = {
-			'productId': productId
-		};
+		// let store = {
+		// 	'productId': productId
+		// };
 		console.log(req.params.id);
-		res.render('products/edit', store);
+		res.render('products/edit', {"productId": productId});
 	})
 
 	.delete((req,res) => {
