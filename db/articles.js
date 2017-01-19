cac28a79f126c34a5064c01a92b076d17bcd1b9b
nms377 <nms377@nyu.edu>
2017-01-18 15:40:15 -1000

    Trying to figure out how to find the urlTitle.

--- db/articles.js
+++ db/articles.js
@@ -2,17 +2,55 @@ module.exports = (function() {
 
 var articlesList = [];
 
+function _all(data){
+	console.log(articlesList);
+	return articlesList;
+}
+
 function _add(data){
 	data.urlTitle = encodeURI(data.title);
 	articlesList.push(data);
-	console.log(articlesList);
+}
+	
+function _getByTitle(data, title){
+		for(let i=0; i < articlesList.length; i++){
+			if(articlesList[i].title === parseInt(title)){
+				console.log(articlesList[i]);
+				return articlesList[i];
+			}
+		}
+}
+
+function _edit(data, title){
+	for(let i=0; i < articlesList.length; i++){
+			if(articlesList[i].title === parseInt(title)){
+				articlesList[i].title = data.title;
+				return articlesList[i];
+			}
+
+			if(articlesList[i].urlTitle === parseInt(title)){
+				articlesList[i].body = data.body;
+				return articlesList[i];
+			}
+		}
+}
+
+function _delete(data, title){
+	for(let i=0; i < articlesList.length; i++){
+		if(articlesList[i].title === parseInt(title)){
+			console.log(articlesList[i]);
+			articlesList.splice(i, 1);
+			return articlesList;
+		}
+	}
 }
 
 	return {
-		// all: _all,
+		all: _all,
 		add: _add,
-		// getByTitle: _getByTitle,
-		// editByTitle: _editByTitle
+		getByTitle: _getByTitle,
+		edit: _edit,
+		delete: _delete
 	};
 
 })();
