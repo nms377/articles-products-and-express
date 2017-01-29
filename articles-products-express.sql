3231ec7459f2a7988548d6c3203788039ac98aaf
nms377 <nms377@nyu.edu>
2017-01-28 20:14:21 -1000

    Renamed my databse from products to articles-products. Also changed my price data type to money, but I'm finding an error when I try to edit the price. Need to parse the dollar symbol..

--- articles-products-express.sql
+++ articles-products-express.sql
@@ -2,17 +2,25 @@ DROP USER IF EXISTS "nicolesandry";
 
 CREATE USER "nicolesandry" WITH PASSWORD 'password';
 
-DROP DATABASE IF EXISTS "products";
+DROP DATABASE IF EXISTS "articles-products";
 
-CREATE DATABASE "products" OWNER "nicolesandry";
+CREATE DATABASE "articles-products" OWNER "nicolesandry";
 
-\c products "nicolesandry";
+\c articles-products "nicolesandry";
 
 DROP TABLE IF EXISTS products;
 
 CREATE TABLE products(
 	id serial NOT NULL PRIMARY KEY,
-	product_name varchar(100) NULL,
-	price integer NULL,
+	product_name varchar(32) NULL,
+	price money NULL,
 	inventory integer NOT NULL
 );
+
+-- CREATE TABLE articles(
+-- 	id serial NOT NULL PRIMARY KEY,
+-- 	title varchar(32) NULL,
+-- 	body varchar(250) NULL,
+-- 	author varchar(64) NULL,
+-- 	url_title text
+-- );
