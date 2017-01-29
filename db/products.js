a6ea7308da06487406512b01e9a86320380a9f2a
nms377 <nms377@nyu.edu>
2017-01-28 20:15:01 -1000

    figuring out how to parse the dollar symbol in my getProductById function.

--- db/products.js
+++ db/products.js
@@ -5,7 +5,7 @@ const pgp = require('pg-promise')();
 const db = pgp({
 	host: 'localhost',
 	port: 5432,
-	database: 'products',
+	database: 'articles-products',
 	user: 'nicolesandry',
 	password: PG_PASS
 });
@@ -14,11 +14,8 @@ module.exports = (function(){
 
 	function _getAllProducts(req, res){
 
-		console.log('db-all', req.body);
-
 		db.any('SELECT * FROM products')
 			.then( result => {
-				console.log('result', result);
 				res.render('products/index', {"productList": result});
 			})
 			.catch( err => {
@@ -28,17 +25,9 @@ module.exports = (function(){
 	}
 
 	function _addProduct(req, res){
-		// data.id = id;
-		// productsArr.push(data);
-		// id++;
-		// console.log('productsArr');
-
-		console.log('db', req.body);
-		console.log(typeof req.body.name);
 
 		db.none(`INSERT INTO products (product_name, price, inventory) VALUES ('${req.body.name}', ${req.body.price}, ${req.body.inventory})`, req.body)
 			.then( add => {
-				console.log('add', add);
 				res.redirect('/products');
 			})
 			.catch( err => {
@@ -49,12 +38,11 @@ module.exports = (function(){
 
 
 	function _getProductById(req, res){
-
-		console.log("getProductById", req.params.id);
+		
 
 		return db.one(`SELECT * FROM products WHERE id = ${req.params.id}`)
 			.then( id => {
-				console.log('/:id/edit', id);
+				console.log('getProductById', id);
 				res.render('products/edit', {"productId": id});
 			})
 			.catch( err => {
@@ -65,46 +53,25 @@ module.exports = (function(){
 
 	function _editProductById(req, res){
 
-		console.log('editProductById', req.body);
-		console.log('editId', req.params.id);
-		console.log('editProduct', req.body.id);
-
-		db.none(`UPDATE products SET product_name = '${req.body.name}' WHERE id = ${req.body.id}`)
+		db.none(`UPDATE products SET product_name = '${req.body.name}', price = ${req.body.price}, inventory = ${req.body.inventory} WHERE id = ${req.body.id}`)
 			.then( (result) => {
-				console.log('check', req.params.id);
-				console.log(result);
 				res.redirect(`../edit`);
 			})
 			.catch( err => {
 				console.log('error', err);
 				console.log('this didn\'t work');
 			});
-	// })
-
-
-		// let productName = data.name;
-		// // will try to create a switch statement to be able to change the price and inventory
-		// // let prodcutPrice = data.price;
-		// // let productInventory = data.inventory;
-
-		// for(let i=0; i < productsArr.length; i++){
-		// 	if(productsArr[i].id === parseInt(id)){
-		// 		productsArr[i].name = productName;
-		// 		return productsArr[i];
-		// 	}
-		// }
-
-		// console.log('updated product list:', productsArr);
 	}
 
-	function _deleteProductById(id){
-		for(let i=0; i < productsArr.length; i++){
-			if(productsArr[i].id === parseInt(id)){
-				productsArr.splice(i, 1);
-			}
-		}
+	function _deleteProductById(req, res){
 
-		console.log('updated product list: ', productsArr);
+		db.none(`DELETE FROM products WHERE id = ${req.params.id}`)
+			.then( () => {
+				res.redirect('/products');
+			})
+			.catch( err => {
+				console.log('Cannot delete product');
+			});
 	}
 
 	return {
