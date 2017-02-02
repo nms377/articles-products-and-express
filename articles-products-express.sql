b0a92a9dcf36474fa812910c6e1b59abda72ee50
nms377 <nms377@nyu.edu>
2017-02-01 19:28:54 -1000

    Uncommented articles table.

--- articles-products-express.sql
+++ articles-products-express.sql
@@ -13,14 +13,14 @@ DROP TABLE IF EXISTS products;
 CREATE TABLE products(
 	id serial NOT NULL PRIMARY KEY,
 	product_name varchar(32) NULL,
-	price money NULL,
+	price integer NULL,
 	inventory integer NOT NULL
 );
 
--- CREATE TABLE articles(
--- 	id serial NOT NULL PRIMARY KEY,
--- 	title varchar(32) NULL,
--- 	body varchar(250) NULL,
--- 	author varchar(64) NULL,
--- 	url_title text
--- );
+CREATE TABLE articles(
+	id serial NOT NULL PRIMARY KEY,
+	title varchar(32) NULL,
+	body varchar(250) NULL,
+	author varchar(64) NULL,
+	url_title varchar(200)
+);
