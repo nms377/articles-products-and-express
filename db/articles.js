b9d957b05d79d7c0c12ff262b3002c9c9bba4430
nms377 <nms377@nyu.edu>
2017-02-01 19:29:46 -1000

    Refactoring my module to include pg promises. Got my GET and POST method to work for /articles.

--- db/articles.js
+++ db/articles.js
@@ -1,40 +1,88 @@
+const PG_PASS = process.env.PG_PASS;
+const pgp = require('pg-promise')();
+
+const dbArticles = pgp({
+	host: 'localhost',
+	port: 5432,
+	database: 'articles-products',
+	user: 'nicolesandry',
+	password: PG_PASS
+});
+
 module.exports = (function() {
 
-var articlesList = [];
+// var articlesList = [];
 
-function _all(data){
-	console.log(articlesList);
-	return articlesList;
+function _getAllArticles(req, res){
+	dbArticles.any('SELECT * FROM articles')
+		.then( result => {
+			res.render('articles/index', {"articlesList": result});
+		})
+		.catch( err => {
+			res.send('Oops...');
+		});
 }
 
-function _add(data){
-	data.urlTitle = encodeURI(data.title);
-	articlesList.push(data);
-	console.log(articlesList);
+function _addArticle(req, res){
+	let newArticle = req.body;
+	let url_title = encodeURI(newArticle.title);
+
+	dbArticles.none(`INSERT INTO articles (title, body, author, url_title) VALUES ('${newArticle.title}', '${newArticle.body}', '${newArticle.author}', '${url_title}')`)
+	.then( articles => {
+		console.log('newArticle', newArticle);
+		res.redirect('/articles');
+	})
+	.catch( err => {
+		console.log('err', err);
+		console.log('newArticle', newArticle);
+		res.redirect('/articles/new');
+	});
 }
 	
-function _getByTitle(data, title){
-	console.log('_getByTitle', title);
-		for(let i=0; i < articlesList.length; i++){
-			if(articlesList[i].title === title){
-				console.log(articlesList[i]);
-				return articlesList[i];
-			}
-		}
+function _getArticleByTitle(req, res){
+	
+	let getArticle = req.params.title;
+
+	console.log('getArticle', getArticle);
+
+	dbArticles.one(`SELECT * FROM articles WHERE title = '${getArticle}'`)
+			.then( article => {
+				console.log('getArticleByTitle', article);
+				res.render('articles/article', {"getArticleByTitle": article});
+			})
+			.catch( err => {
+				console.log('err', err);
+				console.log(getArticle);
+				res.redirect('/articles');
+			});
+
 }
 
-function _edit(data, title){
-	for(let i=0; i < articlesList.length; i++){
-			if(articlesList[i].title === title){
-				articlesList[i].title = data.title;
-				return articlesList[i];
-			}
+function _editArticleByTitle(req, res){
+	
+	let editArticle = req.params.url_title;
+	console.log('url_title', url_title);
 
-			if(articlesList[i].title === title){
-				articlesList[i].body = data.body;
-				return articlesList[i];
-			}
-		}
+	db.none(`UPDATE articles SET product_name = '${editArticle}', price = ${editArticle.title}, inventory = ${editArticle.body} WHERE url_title = ${editArticle.url_title}`)
+			.then( (result) => {
+				res.redirect(`../edit`);
+			})
+			.catch( err => {
+				console.log('error', err);
+				console.log('this didn\'t work');
+			});
+
+	// for(let i=0; i < articlesList.length; i++){
+	// 		if(articlesList[i].title === title){
+	// 			articlesList[i].title = data.title;
+	// 			return articlesList[i];
+	// 		}
+
+	// 		if(articlesList[i].title === title){
+	// 			articlesList[i].body = data.body;
+	// 			return articlesList[i];
+	// 		}
+	// 	}
 }
 
 function _delete(data, title){
@@ -48,10 +96,10 @@ function _delete(data, title){
 }
 
 	return {
-		all: _all,
-		add: _add,
-		getByTitle: _getByTitle,
-		edit: _edit,
+		getAllArticles: _getAllArticles,
+		addArticle: _addArticle,
+		getArticleByTitle: _getArticleByTitle,
+		editArticleByTitle: _editArticleByTitle,
 		delete: _delete
 	};
 
