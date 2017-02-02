792a465b2f61059607cb102c618103e231f7c333
nms377 <nms377@nyu.edu>
2017-02-01 19:29:59 -1000

    Refactoring to include pg-promises.

--- routes/articles.js
+++ routes/articles.js
@@ -15,24 +15,19 @@ router.route('/new')
 router.route('/')
 	//View Product List
 	.get((req,res) => {
-	let articlesList = articles.all();
-	// console.log(req.body);
-	res.render('articles/index', {"articlesList": articlesList});
+		articles.getAllArticles(req, res);
 })
 	.post((req, res) => {
-			let addArticle = articles.add(req.body, req.params);
-			console.log('req.body.urlTitle', req.params);
-			res.redirect('/articles');
+		articles.addArticle(req, res);
 });
 
 router.route('/:title')
 	.get((req, res) => {
-		let articleId = articles.getByTitle(req.body, req.params.title);
-		res.render('artciles', {"articleId": articleId});
+		articles.getArticleByTitle(req, res);
+		// res.render('articles', {"articleId": getArticleByTitle});
 })
 	.put((req, res) => {
 		let editArticle = articles.edit(req.body, req.params.title);
-		res.redirect(303, `/articles/${req.params.title}/edit`);
 })
 	.delete((req, res) => {
 		let deleteArticle = articles.delete(req.body, req.params.title);
@@ -40,8 +35,9 @@ router.route('/:title')
 
 router.route('/articles/:title/edit')
 	.get((req, res) => {
-		let editArticle = articles.edit();
-		res.render('articles/edit', {"editArticle": editArticle});
+		articles.editArticleByTitle(req, res);
+		// let editArticle = articles.edit();
+		// res.render('articles/edit', {"editArticle": editArticle});
 });
 
 module.exports = router;
